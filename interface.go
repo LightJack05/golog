4efc11ed46d7efa0ddd9d016ab3a61149cba17a2
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-08-21 16:53:39 +0200

    Added interface for logging

--- interface.go
+++ interface.go
@@ -1,21 +1,24 @@
 package main
 
 func Debug(message string) {
-
+	writeMessage(LOG_LEVEL_DEBUG, message)
 }
 
 func Info(message string) {
-
+	writeMessage(LOG_LEVEL_INFO, message)
 }
 
 func Warning(message string) {
-
+	writeMessage(LOG_LEVEL_WARNING, message)
 }
 
 func Error(message string) {
-
+	writeMessage(LOG_LEVEL_ERROR, message)
 }
 
-func Fatal(message string) {
-
+func Fatal(message string, panicProgram bool) {
+	writeMessage(LOG_LEVEL_FATAL, message)
+	if panicProgram {
+		panic("Program terminated due to fatal error: " + message)
+	}
 }
